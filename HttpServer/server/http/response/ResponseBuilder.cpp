f271ad0f2eb04c6dc901432da8bf8f0e43c19f7d
alekseyl1992 <alekseyl@list.ru>
2014-09-10 16:49:24 +0400

    ResponseBuilder workarounds

--- HttpServer/server/http/response/ResponseBuilder.cpp
+++ HttpServer/server/http/response/ResponseBuilder.cpp
@@ -1,7 +1,48 @@
 #include "ResponseBuilder.h"
 
-Response ResponseBuilder::build()
+const ResponseBuilder &ResponseBuilder::getInstance()
+{
+    if (instance == nullptr)
+        instance = new ResponseBuilder();
+
+    return *instance;
+}
+
+
+ResponseBuilder::ResponseBuilder()
+{
+    //fill the maps
+}
+
+Response ResponseBuilder::build(unsigned int code, std::string dataExtension, const char *data, unsigned long size)
 {
     //TODO:
+    //build headers
+
+    //build data
+
     return Response();
+}
+
+
+Response ResponseBuilder::buildDefaultPage(unsigned int code, std::string info)
+{
+    switch (code) {
+    case 200:
+        return build(200, "", info.c_str(), info.size());
+        break;
+    case 403:
+        return build(403, "", info.c_str(), info.size());
+        break;
+    case 404:
+        return build(404, "", info.c_str(), info.size());
+        break;
+    case 500:
+        return build(500, "", info.c_str(), info.size());
+        break;
+
+    default:
+        return build(500, "", info.c_str(), info.size());
+        break;
+    }
 }
--- HttpServer/server/http/response/ResponseBuilder.h
+++ HttpServer/server/http/response/ResponseBuilder.h
@@ -2,13 +2,25 @@
 #define REQUESTPARSER_H
 
 #include <string>
+#include <map>
 #include "Response.h"
 
 class ResponseBuilder
 {
 public:
-    ResponseBuilder() = delete;
-    static Response build();
+    const ResponseBuilder &getInstance();
+
+    Response build(unsigned int code, std::string dataExtension, const char *data, unsigned long size);
+    Response buildDefaultPage(unsigned int code, std::string info = "");
+
+private:
+    ResponseBuilder();
+    ResponseBuilder(const &ResponseBuilder) = delete;
+
+    ResponseBuilder *instance = nullptr;
+
+    std::map<std::string, std::string> extToMime;
+    std::map<std::string, std::string> defaultPageData;
 };
 
 #endif // REQUESTPARSER_H
